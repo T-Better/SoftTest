3fd0da02c7e130b3536f3751d1da27d083dd40d1
claren <www.1121008136@foxmail.com>
2022-04-07 13:15:27 +0800

    20220407 增加selenium复习

---
+++ B01_14 UI自动化/B01_14 09 Selenium/anki_se_20220407.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+
+p1 = r'D:\Program Files\pagetest\注册A.html'
+# 获取用户名输入框的大小
+# .size
+"""
+打开‘注册A.html’页面，完成以下操作
+1). 填写注册信息
+2). 截图保存 driver.get_screenshot_as_file('paht+name')
+"""
+
+
+
