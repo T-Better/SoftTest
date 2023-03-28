044190afd1e2e37875ab84645ee26f90e827e726
claren <www.1121008136@foxmail.com>
2023-03-28 20:48:32 +0800

    20230328 今天复习了88张卡片，务必这周全复习完

---
+++ B01_02 常用Python包学习/B01_02 08 Paramiko/Codes/2023/03月/anki_上传_281924.py
@@ -0,0 +1,19 @@
+# import paramiko
+# TODO
+
+rp = r'\home\python\致您的一封信.doc'
+lp = r'D:\Work\C-人行-浪潮\C00-浪潮\C03_文档\致您的一封信.doc'
+
+# 从widnows端上传文件到linux服务器
+# TODO
+"""
+t = paramiko.Transport('192.168.146.128',22)
+t.connect(username='python', password='chuanzhi')
+sftp = paramiko.SFTPClient.from_transport(t)
+sftp.put(lp, rp)
+"""
+
+t.close()
+
+
+
