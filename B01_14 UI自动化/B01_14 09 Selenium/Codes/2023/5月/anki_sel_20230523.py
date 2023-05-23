270dd8cb87477acbf78fef60be25651b7442a608
claren <www.1121008136@foxmail.com>
2023-05-23 17:48:01 +0800

    anki 20230523

---
+++ B01_14 UI自动化/B01_14 09 Selenium/Codes/2023/5月/anki_sel_20230523.py
@@ -0,0 +1,28 @@
+
+from selenium import webdriver
+import time
+
+cdp = r'D:\Program Files\pagetest\注册A.html'
+zh = r'https://www.zhihu.com/signin?next=%2F'
+driver = webdriver.Chrome()
+driver.get(cdp)
+
+# 将滚动条滑到最底层
+js1 = "window.scrollTo(1000,1000)"
+driver.execute_script(js1)
+time.sleep(1)
+
+driver.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
