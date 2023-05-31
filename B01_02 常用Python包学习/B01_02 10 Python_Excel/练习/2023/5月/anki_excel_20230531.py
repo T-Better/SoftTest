f3b50503f59f01023a6d96c94451ac38947f45c5
claren <www.1121008136@foxmail.com>
2023-05-31 09:38:09 +0800

    20230531 anki复习 over

---
+++ B01_02 常用Python包学习/B01_02 10 Python_Excel/练习/2023/5月/anki_excel_20230531.py
@@ -0,0 +1,26 @@
+
+import openpyxl as opx
+
+p = r'D:\Git_Reps\SoftTest\Soft_test\SoftTest\B01_02 常用Python包学习\B01_02 10 Python_Excel\002 Docs'
+f1 = r'\anki_1678953594.xlsx'
+f2 = r'\anki_设置行高列高_20230316.xlsx'
+
+wb1 = opx.load_workbook(p+f1)
+ws1 = wb1['1号']
+
+# 标题水平垂直居中且自动换行
+ali1 = opx.styles.Alignment(horizontal='center',vertical='center',wrap_text=True)
+for h in ws1['A1:J1']:
+    for c in h:
+        c.alignment = ali1
+
+wb1.save(p+f1)
+
+
+
+
+
+
+
+
+
