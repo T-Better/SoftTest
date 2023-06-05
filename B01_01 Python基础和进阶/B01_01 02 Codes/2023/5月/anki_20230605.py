deaa0eb70ed060dd59c11445e3154940f9101c70
claren <www.1121008136@foxmail.com>
2023-06-05 18:00:50 +0800

    20230605 anki复习 over

---
+++ B01_01 Python基础和进阶/B01_01 02 Codes/2023/5月/anki_20230605.py
@@ -0,0 +1,15 @@
+# 定义 cat 类，属性为eat何drink
+class cat(object):
+    def eat(self):
+            print("汤姆爱吃鱼")
+
+
+    def drink(self):
+            print("汤姆爱喝水")
+
+# 将cat类实例化为lazy_cat 对象
+lazy_cat = cat()
+# 调用对象的 eat 方法
+lazy_cat.eat()
+# 调用对象的 drink 方法
+lazy_cat.drink()
---
+++ B01_02 常用Python包学习/B01_02 10 Python_Excel/练习/2023/6月/anki_excel_20230605.py
@@ -0,0 +1,32 @@
+import openpyxl as opx
+
+p = r'D:\Git_Reps\SoftTest\Soft_test\SoftTest\B01_02 常用Python包学习\B01_02 10 Python_Excel\002 Docs'
+f1 = r'\anki_1678953594.xlsx'
+f2 = r'\anki_设置行高列高_20230316.xlsx'
+
+wb1 = opx.load_workbook(p+f1)
+ws1 = wb1['1号']
+# 第一行 双实线 黑色
+s1 = opx.styles.Side(color='000000', style='double')
+b1 = opx.styles.Border(left=s1,right=s1,top=s1,bottom=s1)
+for h in ws1['A1':'J1']:
+    for c in h:
+        c.border = b1
+
+# 后面几行
+# TODO
+# TODO
+for t in ws1['A2':'J10']:
+    for c in t:
+        pass # TODO
+
+wb1.save(p+f1)
+
+
+
+
+
+
+
+
+
