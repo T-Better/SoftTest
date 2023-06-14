86f994f4231480465b204ac60a0ff79ff121cec3
claren <www.1121008136@foxmail.com>
2023-06-14 10:59:47 +0800

    20230614 网页计算器自动化测试：第一版 bate1

--- B01_66 作品/14 UI自动化测试/14.2 网页计算器自动化测试/My_Codes_V1/testcases/test_cal_page.py
+++ B01_66 作品/14 UI自动化测试/14.2 网页计算器自动化测试/My_Codes_V1/testcases/test_cal_page.py
@@ -10,7 +10,7 @@ class TestCal():
     计算测试 相当于业务层
     """
     @pytest.mark.parametrize('a,b,expect', get_jsondata('caladd.json'))
-    @allure.story('测试增加按钮，共4个case')
+    @allure.story('测试增加按钮，共4个case,3p1f')
     def test_add(self, a, b, expect):
         """
         expect:我们预期值
@@ -21,28 +21,29 @@ class TestCal():
         # print(add_res)
         assert add_res == str(expect)
 
-    # @pytest.mark.parametrize('a,b,expect',get_jsondata('calsubtract.json'))
-    # def test_subtract(self, a, b, expect):
-    #     """测试减法"""
-    #     subtract_res = CalElement().subtract(a,b)
-    #     print(subtract_res)
-    #     assert subtract_res == str(expect)
-    #
-    # @pytest.mark.parametrize('a,b,expect',get_jsondata('calmultiply.json'))
-    # def test_multiply(self, a, b, expect):
-    #     """测试乘法"""
-    #     multiply_res = CalElement().multiply(a, b)
-    #     print(multiply_res)
-    #     assert multiply_res == str(expect)
-    #
-    # @pytest.mark.parametrize('a,b,expect', get_jsondata('caldevide.json'))
-    # def test_devide(self, a, b, expect):
-    #     """测试除法"""
-    #     devide_res = CalElement().devide(a, b)
-    #     print(devide_res)
-    #     assert devide_res == str(expect)
+    @allure.story('测试减按钮，共4个case,3p1f')
+    @pytest.mark.parametrize('a,b,expect',get_jsondata('calsubtract.json'))
+    def test_subtract(self, a, b, expect):
+        """测试减法"""
+        subtract_res = CalElement().subtract(a,b)
+        print(subtract_res)
+        assert subtract_res == str(expect)
 
+    @allure.story('测试乘法按钮，共4个case,3p1f')
+    @pytest.mark.parametrize('a,b,expect',get_jsondata('calmultiply.json'))
+    def test_multiply(self, a, b, expect):
+        """测试乘法"""
+        multiply_res = CalElement().multiply(a, b)
+        print(multiply_res)
+        assert multiply_res == str(expect)
 
+    @allure.story('测试除法按钮，共4个case,3p1f')
+    @pytest.mark.parametrize('a,b,expect', get_jsondata('caldevide.json'))
+    def test_devide(self, a, b, expect):
+        """测试除法"""
+        devide_res = CalElement().devide(a, b)
+        print(devide_res)
+        assert devide_res == str(expect)
 
 
 if __name__ == "__main__":
@@ -50,7 +51,8 @@ if __name__ == "__main__":
     # testcal.test_add()
     testfile = BASE_DIR + r'\testcases\test_cal_page.py'
     pytest.main(['-s', testfile,])
-    # pytest -s ./testcases/test_cal_page.py --alluredir ./report
+    # pytest -s ./testcases/test_cal_page.py --alluredir ./result/tmp  # 生成allure测试报告.json文件
+    # allure serve ./result/tmp  # 根据报告.json文件生成可查看的在线测试报告
 
 
 
