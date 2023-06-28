e527ce8c0d42c89e2114e9c98c3b2f18c7aa417d
claren <www.1121008136@foxmail.com>
2023-06-28 16:51:24 +0800

    20230628 anki over

--- B01_04 日志/Codes/日志常用练习/anki_loguru/test_dog.py
+++ B01_04 日志/Codes/日志常用练习/anki_loguru/test_dog.py
@@ -1,5 +1,5 @@
 # test_dog.py
-# TODO
+
 
 
 class test_dog():
@@ -10,6 +10,7 @@ class test_dog():
     # TODO
     def show_name(self):
         print(self.name)
+
 
 d = test_dog()
 d.set_name('旺财')
