66d8a80bec74017f2aae6efba794482acf4b4f3c
claren <www.1121008136@foxmail.com>
2023-06-30 19:12:04 +0800

    20230630 anki over

---
+++ B01_02 常用Python包学习/B01_02 10 Python_Excel/练习/2023/6月/anki_excel_20230630.py
@@ -0,0 +1,26 @@
+import openpyxl as opx
+
+p = r'D:\Git_Reps\SoftTest\Soft_test\SoftTest\B01_02 常用Python包学习\B01_02 10 Python_Excel\002 Docs'
+f1 = r'\anki_1678953594.xlsx'
+f2 = r'\anki_设置行高列高_20230316.xlsx'
+
+wb1 = opx.load_workbook(p + f1)
+ws2 = wb1['2号']
+
+# 移动1-3列1-3行向下移动两行，向右移动一列
+ws2.move_range('A1,C3',rows=2,cols=1)
+
+wb1.save(p + f1)
+
+
+
+
+
+
+
+
+
+
+
+
+
