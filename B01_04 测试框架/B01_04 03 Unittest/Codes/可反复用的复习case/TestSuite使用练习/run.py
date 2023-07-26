294756da85a7336e1f501f08c15d94943a1ee303
claren <www.1121008136@foxmail.com>
2023-07-26 10:40:23 +0800

    20230726 复习 over

--- B01_04 测试框架/B01_04 03 Unittest/Codes/可反复用的复习case/TestSuite使用练习/run.py
+++ B01_04 测试框架/B01_04 03 Unittest/Codes/可反复用的复习case/TestSuite使用练习/run.py
@@ -2,13 +2,4 @@
 # 生成 HTMLTestRunner 测试报告
 # 实现使用TestSuite方法将 test01.py中的my_test01类的test_01方法、test02.py中所有test开头的方法用例批量执行；
 # TODO
-import unittest
-import test01,test02
-
-suite = unittest.TestSuite()
-suite.addTest(test01.my_test01('test_01'))
-suite.addTest(unittest.makeSuite(test02.my_test02))
-
-runner = unittest.TextTestRunner()
-runner.run(suite)
 
