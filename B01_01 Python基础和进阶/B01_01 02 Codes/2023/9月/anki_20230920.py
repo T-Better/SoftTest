6401bd78908cb1269f3945a690b7c5c6b5a45d85
Claren_PC <claren_lau@163.com>
2023-09-20 18:09:35 +0800

    20230920

---
+++ B01_01 Python基础和进阶/B01_01 02 Codes/2023/9月/anki_20230920.py
@@ -0,0 +1,16 @@
+import yaml
+
+data = {"s_data":{"test1":"hello1"},"sdata2":{"name":"newroman_1"}}
+
+with open('data_3.yaml', 'w') as f:
+    yaml.dump(data, f, encoding='utf8', allow_unicode=True)
+
+
+
+
+
+
+
+
+
+
