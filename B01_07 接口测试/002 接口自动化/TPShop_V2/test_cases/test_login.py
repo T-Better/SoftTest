64ad9d982a13ececa588b9a9f58b4c3862d8edd3
claren <www.1121008136@foxmail.com>
2024-03-21 20:11:19 +0800

    接口自动化 TPShop V2

--- B01_07 接口测试/002 接口自动化/TPShop_V2/test_cases/test_login.py
+++ B01_07 接口测试/002 接口自动化/TPShop_V2/test_cases/test_login.py
@@ -1,21 +1,22 @@
 import requests
 import pytest
+from api.login import LoginApi
 
 
 
 class TestLogin():
-
     # case1:登陆成功
     def test_login_success(self):
         # 获取验证码
         response = self.login_api.get_login_verifycode(self.session)
         # 断言验证码返回的是不是图片
-        self.assert("image",response.header.get("Content-Type"))
+        assert("image",response.header.get("Content-Type"))
 
         # 登录
 
 
 
 if __name__ == "__main__":
-    # pytest.main(['-s'])
-    testlogin(login_url, data=login_data)
+    login_api = LoginApi()
+    pytest.main(['-s'])
+    # testlogin(login_url, data=login_data)
