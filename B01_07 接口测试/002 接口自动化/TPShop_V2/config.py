38c3e72f946f4efc47fd37fb0ff984c22450aa24
claren <www.1121008136@foxmail.com>
2024-04-17 21:04:35 +0800

    111

--- B01_07 接口测试/002 接口自动化/TPShop_V2/config.py
+++ B01_07 接口测试/002 接口自动化/TPShop_V2/config.py
@@ -1,3 +1,38 @@
+import os
+
+
+# 获取文件的绝对路径
+abs_path = os.path.abspath(__file__)
+print(abs_path)
+
+# 日志路径 os.sep就是系统默认分隔符，windows是\
+_log_path = abs_path + os.sep + 'log'
+
+# 报告路径
+_report_path = abs_path + os.sep + 'reports'
+
+# 数据库信息配置
+DB_CONFIG = {
+    "host":"localhost",
+    "user":"root",
+    "password":"123456",
+    "port":3306,
+    "database":"tpshop2.0"
+}
+
+# 返回日志目录
+def get_log_path():
+    return _log_path
+
+# 返回报告目录
+def get_report_path():
+    return _report_path
+
+# 测试代码
+if __name__ == "__main__":
+    print(get_log_path())
+    print(get_report_path())
+
 
 
 
--- B01_07 接口测试/002 接口自动化/TPShop_V2/testcases/test_login.py
+++ B01_07 接口测试/002 接口自动化/TPShop_V2/testcases/test_login.py
@@ -6,17 +6,21 @@ from api.login import LoginApi
 
 class TestLogin():
     # case1:登陆成功
+    def __init__(self):
+        self.login_api = LoginApi()
+        self.session = requests.Session()
+
     def test_login_success(self):
         # 获取验证码
         response = self.login_api.get_login_verifycode(self.session)
+        print("1")
         # 断言验证码返回的是不是图片
-        assert("image",response.header.get("Content-Type"))
-
+        assert response.header.get("Content-Type") == "image"
+        print("2")
         # 登录
 
 
 
 if __name__ == "__main__":
-    login_api = LoginApi()
-    pytest.main(['-s'])
+    pytest.main(['-s','-v','test_login.py'])
     # testlogin(login_url, data=login_data)
